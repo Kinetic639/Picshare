41fa7e2e1f184598f2d722dec79882c4c160610f
Kinetic639 <michal.stepien36@gmail.com>
2020-08-01 13:23:11 +0200

    useStorage hook

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBsDeyUzwbIO5beb0tRwAygzVCEfE2LC1Q",
+  authDomain: "picshare-f92cd.firebaseapp.com",
+  databaseURL: "https://picshare-f92cd.firebaseio.com",
+  projectId: "picshare-f92cd",
+  storageBucket: "picshare-f92cd.appspot.com",
+  messagingSenderId: "555229151675",
+  appId: "1:555229151675:web:edfa11e2bb3f78ddda049e",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+export { projectFirestore, projectStorage };
+
